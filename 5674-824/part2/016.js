448c14f00545b240469eb16af73d8efff9aa4718
moon-snow <75515312+jiseunghun@users.noreply.github.com>
2022-01-20 00:30:22 +0900

    test

--- 5674-824/part2/016.js
+++ 5674-824/part2/016.js
@@ -1,4 +1,4 @@
-console.log(Infinity); /* Infinity */ 
+console.log(Infinity); /* Infinity */
 console.log(1 / Infinity); /* 0 */
 console.log(0 / 0); /* NaN */
 console.log(Infinity - Infinity); /* NaN */
