d3dfdf7cfd76fc93ef318be13e122272ddbf25cc
moon-snow <75515312+jiseunghun@users.noreply.github.com>
2022-03-12 18:00:16 +0900

    study

--- Do_it/js-basic-new-main/10/js/sol-2.js
+++ Do_it/js-basic-new-main/10/js/sol-2.js
@@ -1,9 +1,17 @@
-    function changeBg() {
-      var x = Math.floor(Math.random() * 256);
-      var y = Math.floor(Math.random() * 256);
-      var z = Math.floor(Math.random() * 256);
-      var bgColor = "rgb(" + x + "," + y + "," + z + ")";
-        document.body.style.background = bgColor;
-    }
+function changeBg() {
+  var x = Math.floor(Math.random() * 256);
+  var y = Math.floor(Math.random() * 256);
+  var z = Math.floor(Math.random() * 256);
+  var bgColor = "rgb(" + x + "," + y + "," + z + ")";
+  document.body.style.background = bgColor;
+}
 
-    changeBg();
+changeBg();
+
+reload_btn = document.querySelectorAll("#container button");
+
+function reload_page() {
+  window.location.reload();
+}
+
+reload_btn[0].onclick = reload_page;
